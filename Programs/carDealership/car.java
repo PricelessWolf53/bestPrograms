2390aeb68e73bcf02b2292ffde7980b2d19d57e2
PricelessWolf53 <90570427+PricelessWolf53@users.noreply.github.com>
2022-06-07 00:06:25 -0400

    Create car.java

---
+++ Programs/carDealership/car.java
@@ -0,0 +1,14 @@
+package garage;
+
+public class Car {
+	String model;
+	String color;
+	String modelPackage;
+	
+	public Car(String model, String color, String modelPackage)
+	{
+		this.model = model;
+		this.color = color;
+		this.modelPackage = modelPackage;
+	}
+}
