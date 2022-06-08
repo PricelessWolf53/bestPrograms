cc13c498aa8e4d757da4ac072919ae37b42e64fb
PricelessWolf53 <90570427+PricelessWolf53@users.noreply.github.com>
2022-06-07 23:19:52 -0400

    Update car.java

--- Programs/carDealership/car.java
+++ Programs/carDealership/car.java
@@ -1,10 +1,11 @@
 package garage;
 
 public class Car {
+	//instance variables initialized in the constructor
 	String model;
 	String color;
 	String modelPackage;
-	
+	//Constructor that initilaizes variables model, color, modelPackage
 	public Car(String model, String color, String modelPackage)
 	{
 		this.model = model;
